b0a6fd8aa7e1c488b757adb57b1fdc2ffcd2e8d9
tino <tkoumapl@email.essex.edu>
2025-04-08 17:25:54 -0400

    building_appointment_suite

--- tests/test_appointment_suite.py
+++ tests/test_appointment_suite.py
@@ -5,11 +5,13 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 
-
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
 ## combo_facility
 
 pytest.mark.usefixtures("setup_driver")
 class TestAppoinmentSuite:
+
+    def test_003_valid_appointment_schedulling(self):
+
 
