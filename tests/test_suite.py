9c79b2a4bb5a7f13afb0a5f2cfcc1bc008a7e245
tino <tkoumapl@email.essex.edu>
2025-04-09 22:01:28 -0400

    added two more test

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -15,17 +15,17 @@ class TestLoginSuite:
 
     @pytest.mark.order(1)
     @pytest.mark.smoke
-    def test_001_invalid_login(self):
+    def test_001_invalid_login_empty_fields(self):
         home_page = HomePage(self.driver, self.wait)  ### homepage initialization
         home_page.click_make_appointment_button()
         login_page = LoginPage(self.driver, self.wait)  ## loginpage initialization
 
-        h2_tag = self.driver.find_element(By.TAG_NAME, 'h2')  # checking to make login indeed showed up
+        h2_tag = self.driver.find_element(By.TAG_NAME, 'h2')  # checking to make login indeed showed up as a title
         assert h2_tag.text == "Login", f"Expected 'Login', but got {h2_tag.text}"
         time.sleep(3)
         ##### starting to enter login information
-        login_page.click_username_field("John Doe")
-        login_page.click_password_field("ThisIsNot")
+        login_page.click_username_field("")
+        login_page.click_password_field("")
         time.sleep(2)
         time.sleep(2)
         login_page.click_login_button()
@@ -34,11 +34,50 @@ class TestLoginSuite:
         assert h2_tag_2.text == "Login failed! Please ensure the username and password are valid.", f"expected 'Login failed! Please ensure the username and password are valid.' but got {h2_tag_2.text}"
         time.sleep(5)
 
-   ## @pytest.mark.dependency(name="login_success")
-
     @pytest.mark.order(2)
+    def test_002_invalid_login_username(self):
+        home_page = HomePage(self.driver, self.wait)  ### homepage initialization
+        home_page.click_make_appointment_button()
+        login_page = LoginPage(self.driver, self.wait)  ## loginpage initialization
+
+        h2_tag = self.driver.find_element(By.TAG_NAME, 'h2')  # checking to make login indeed showed up
+        assert h2_tag.text == "Login", f"Expected 'Login', but got {h2_tag.text}"
+        time.sleep(3)
+        ##### starting to enter login information
+        login_page.click_username_field("Jojo")
+        login_page.click_password_field("ThisIsNotAPassword")
+        time.sleep(2)
+        time.sleep(2)
+        login_page.click_login_button()
+        time.sleep(6)
+        h2_tag_3 = self.driver.find_element(By.CSS_SELECTOR, '.lead.text-danger')  ## locating the second element
+        assert h2_tag_3.text == "Login failed! Please ensure the username and password are valid.", f"expected 'Login failed! Please ensure the username and password are valid.' but got {h2_tag_3.text}"
+        time.sleep(5)
+
+    @pytest.mark.order(3)
+    def test_003_invalid_login_password(self):
+        home_page = HomePage(self.driver, self.wait)  ### homepage initialization
+        home_page.click_make_appointment_button()
+        login_page = LoginPage(self.driver, self.wait)  ## loginpage initialization
+
+        h2_tag = self.driver.find_element(By.TAG_NAME, 'h2')  # checking to make login indeed showed up
+        #assert h2_tag.text == "Login", f"Expected 'Login', but got {h2_tag.text}"
+        time.sleep(3)
+        ##### starting to enter login information
+        login_page.click_username_field("John Doe")
+        login_page.click_password_field("ThisIsNotnnnnn")
+        time.sleep(2)
+        time.sleep(2)
+        login_page.click_login_button()
+        time.sleep(6)
+        h2_tag_4 = self.driver.find_element(By.CSS_SELECTOR, '.lead.text-danger')  ## locating the second element
+        assert h2_tag_4.text == "Login failed! Please ensure the username and password are valid.", f"expected 'Login failed! Please ensure the username and password are valid.' but got {h2_tag_4.text}"
+        time.sleep(5)
+
+
+    @pytest.mark.order(4)
     @pytest.mark.regression
-    def test_002_valid_login(self):
+    def test_004_valid_login(self):
         home_page = HomePage(self.driver , self.wait)    ### homepage initialization
         home_page.click_make_appointment_button()
         login_page = LoginPage(self.driver, self.wait ) ## loginpage initialization
@@ -52,15 +91,16 @@ class TestLoginSuite:
         time.sleep(2)
         login_page.click_login_button()
         time.sleep(6)
-        h2_tag_2 = self.driver.find_element(By.TAG_NAME, 'h2') ## locating the second element
+        h2_tag_5 = self.driver.find_element(By.TAG_NAME, 'h2') ## locating the second element
 
-        assert h2_tag_2.text == "Make Appointment", f"expected 'Make Appointment' but got {h2_tag_2.text}"
+        assert h2_tag_5.text == "Make Appointment", f"expected 'Make Appointment' but got {h2_tag_5.text}"
         time.sleep(1)
 
     @pytest.mark.smoke
-    @pytest.mark.order(3)
-    def test_003_invalid_appointment_scheduling(self):
+    @pytest.mark.order(5)
+    def test_005_invalid_appointment_scheduling_no_calender_selected(self):
         appointment = AppointmentPage(self.driver, self.wait)
+        self.driver.refresh()
         appointment.click_facility_dropdown()
         time.sleep(4)
         appointment.select_apply_for_hospital_readmission_checkmark()
@@ -70,12 +110,31 @@ class TestLoginSuite:
         time.sleep(4)
         appointment.click_book_appointment()
         time.sleep(5)
-        h2_tag_3 = self.driver.find_element(By.TAG_NAME, 'h2') ## select a locator for text on top of page
-        assert h2_tag_3.text == "Make Appointment", f"expected 'Make Appointment' but got {h2_tag_3.text}"
+        h2_tag_6 = self.driver.find_element(By.TAG_NAME, 'h2') ## select a locator for text on top of page
+        assert h2_tag_6.text == "Make Appointment", f"expected 'Make Appointment' but got {h2_tag_6.text}"
+
+    @pytest.mark.smoke
+    @pytest.mark.order(6)
+    def test_006_invalid_appointment_scheduling_zero_value_inserted(self):
+        appointment = AppointmentPage(self.driver, self.wait)
+        self.driver.refresh()
+        appointment.click_facility_dropdown()
+        time.sleep(4)
+        appointment.select_apply_for_hospital_readmission_checkmark()
+        appointment.click_medicare_radio_button()
+        appointment.calendar("00/00/0000")
+        appointment.write_in_comment_box("hello my name is tino i would like to schedule an appointment")
+        time.sleep(4)
+        appointment.click_book_appointment()
+        time.sleep(5)
+        h2_tag_7 = self.driver.find_element(By.TAG_NAME, 'h2')  ## select a locator for text on top of page
+        assert h2_tag_7.text == "Make Appointment", f"expected 'Make Appointment' but got {h2_tag_7.text}"
+
+
 
     @pytest.mark.regression
-    @pytest.mark.order(4)
-    def test_004_valid_appointment_scheduling(self):
+    @pytest.mark.order(7)
+    def test_007_valid_appointment_scheduling(self):
         appointment = AppointmentPage(self.driver, self.wait)
         self.driver.refresh()  ## need to reload
         appointment.click_facility_dropdown() ## Selecting
@@ -87,8 +146,8 @@ class TestLoginSuite:
         time.sleep(4)
         appointment.click_book_appointment()
         time.sleep(5)
-        h2_tag_4 = self.driver.find_element(By.TAG_NAME, 'h2') ## after valid scheduling Appointment Confirmation
-        assert h2_tag_4.text == "Appointment Confirmation", f"expected 'Appointment Confirmation' but got {h2_tag_4.text}" ## making sure we the test pass
+        h2_tag_8 = self.driver.find_element(By.TAG_NAME, 'h2') ## after valid scheduling Appointment Confirmation
+        assert h2_tag_8.text == "Appointment Confirmation", f"expected 'Appointment Confirmation' but got {h2_tag_8.text}" ## making sure we the test pass
         redirect = self.driver.find_element(By.XPATH,"//a[normalize-space()='Go to Homepage']") ## got to home element
         redirect.click() ##
 
